abf158ed87525403b7ae9e0fb0eef9b8e715c4f9
devilts <s24saltmarsht@lordgrey.org.uk>
2025-09-05 11:18:04 +0100

    data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+{
+  "users": {
+    "mainadmin": {
+      "password": "7d78f... (SHA256 of Sunny2024)",
+      "email": "admin@example.com",
+      "role": "admin",
+      "messages": []
+    }
+  },
+  "groups": {}
+}
